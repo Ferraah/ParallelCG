9a6e6051e1ccc56bd75b44ac7fd1d2216a0391ac
ekki <e.steinmacher.e@gmail.com>
2024-03-10 12:59:32 +0100

    Updated plots

--- sandbox/Ekki/conjugate_gradients-main/plotting/matrix_scaling/plot_su.py
+++ sandbox/Ekki/conjugate_gradients-main/plotting/matrix_scaling/plot_su.py
@@ -10,8 +10,8 @@ def plot_data(file_path, title):
 
     plt.rcParams['text.usetex'] = True
     plt.rcParams.update({'font.size': 13.5})
-    # plt.yscale('log')
-    # plt.xscale('log')
+    plt.yscale('log')
+    plt.xscale('log')
     # Load data from the text file using numpy
     data = np.loadtxt(file_path, delimiter='\t', skiprows=0)
 
@@ -27,8 +27,8 @@ def plot_data(file_path, title):
     popt2, pcov2 = curve_fit(parabula, size, column2)
     popt3, pcov3 = curve_fit(parabula, size, column3)
     # plot fit func
-    plt.plot(size_fit, parabula(size_fit, *popt2), linewidth=0.9, color='tab:green', label=rf'Fit func parallel: $a \cdot x^2$, $a = ${np.format_float_scientific(popt2[0], precision=3, exp_digits=1)}')
-    plt.plot(size_fit, parabula(size_fit, *popt3), linewidth=0.9, color='tab:red', label=rf'Fit func sequential: $a \cdot x^2$, $a = ${np.format_float_scientific(popt3[0], precision=3, exp_digits=1)}')
+    plt.plot(size_fit, parabula(size_fit, *popt2), linewidth=0.9, color='tab:green', label=rf'Fit func parallel: $a \cdot n^2$, $a = ${np.format_float_scientific(popt2[0], precision=3, exp_digits=1)}')
+    plt.plot(size_fit, parabula(size_fit, *popt3), linewidth=0.9, color='tab:red', label=rf'Fit func sequential: $a \cdot n^2$, $a = ${np.format_float_scientific(popt3[0], precision=3, exp_digits=1)}')
 
     # Plot the data
     plt.plot(size, column2, label='Parallel time', linewidth=0.9, color='green', marker='o')
@@ -42,11 +42,11 @@ def plot_data(file_path, title):
     for i, txt in enumerate(column3):
         plt.annotate(f'{txt:.2f}', (size[i], column3[i]), textcoords="offset points", xytext=(0,10), ha='center')
 
-    plt.xlabel('Size of the Problem')
-    plt.ylabel('Time in seconds')
+    plt.xlabel('Matrix size')
+    plt.ylabel('Walltime (s)')
     plt.title(title)
     plt.legend()
-    plt.savefig(file_path+'.png')
+    plt.savefig(file_path+'V1'+'.png')
     plt.show()
 
 if __name__ == "__main__":
